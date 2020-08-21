72b4cf01879f71fac410012464439e75da2887a6
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-21 11:29:42 -0700

    seed State data.

---
+++ Parks/Migrations/20200821182817_SeedStateData.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parks.Migrations
+{
+    public partial class SeedStateData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Nationals",
+                keyColumn: "NationalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Nationals",
+                keyColumn: "NationalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Nationals",
+                keyColumn: "NationalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Nationals",
+                keyColumn: "NationalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Nationals",
+                keyColumn: "NationalId",
+                keyValue: 5);
+
+            migrationBuilder.InsertData(
+                table: "States",
+                columns: new[] { "StateId", "StateName", "StateParkName", "StateParkYear" },
+                values: new object[,]
+                {
+                    { 1, "New York", "Niagara Falls State Park", 1885 },
+                    { 2, "Michigan", "Porcupine Mountains Wilderness State Park", 1944 },
+                    { 3, "Oregon", "Ecola State Park", 1932 },
+                    { 4, "Minnesota", "Tettegouche State Park", 1979 },
+                    { 5, "Nevada", "Valley of Fire State Park", 1935 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 5);
+
+            migrationBuilder.InsertData(
+                table: "Nationals",
+                columns: new[] { "NationalId", "NationalName", "NationalParkName", "NationalParkYear" },
+                values: new object[,]
+                {
+                    { 1, "USA", "Glacier National Park", 1910 },
+                    { 2, "USA", "Grand Canyon National Park", 1919 },
+                    { 3, "USA", "Crater Lake National Park", 1902 },
+                    { 4, "USA", "Yellowstone National Park", 1872 },
+                    { 5, "USA", "Yosemite National Park", 1890 }
+                });
+        }
+    }
+}
--- Parks/Models/ParksContext.cs
+++ Parks/Models/ParksContext.cs
@@ -14,13 +14,13 @@ namespace Parks.Models
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-        builder.Entity<National>()
+        builder.Entity<State>()
             .HasData(
-                new National { NationalId = 1, NationalParkName = "Glacier National Park", NationalName = "USA", NationalParkYear = 1910 },
-                new National { NationalId = 2, NationalParkName = "Grand Canyon National Park", NationalName = "USA", NationalParkYear = 1919 },
-                new National { NationalId = 3, NationalParkName = "Crater Lake National Park", NationalName = "USA", NationalParkYear = 1902 },
-                new National { NationalId = 4, NationalParkName = "Yellowstone National Park", NationalName = "USA", NationalParkYear = 1872 },
-                new National { NationalId = 5, NationalParkName = "Yosemite National Park", NationalName = "USA", NationalParkYear = 1890 }
+                new State { StateId = 1, StateParkName = "Niagara Falls State Park", StateName = "New York", StateParkYear = 1885 },
+                new State { StateId = 2, StateParkName = "Porcupine Mountains Wilderness State Park", StateName = "Michigan", StateParkYear = 1944 },
+                new State { StateId = 3, StateParkName = "Ecola State Park", StateName = "Oregon", StateParkYear = 1932 },
+                new State { StateId = 4, StateParkName = "Tettegouche State Park", StateName = "Minnesota", StateParkYear = 1979 },
+                new State { StateId = 5, StateParkName = "Valley of Fire State Park", StateName = "Nevada", StateParkYear = 1935 }
                 
             );
         }
