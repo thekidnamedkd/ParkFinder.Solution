96584bd772f753a8cf26ffdd0c7dad00386b59d3
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-21 12:49:12 -0700

    fix error with national parks not returning in postman.

--- Parks/Controllers/NationalsController.cs
+++ Parks/Controllers/NationalsController.cs
@@ -17,43 +17,43 @@ namespace Parks.Controllers
       _db = db;
     }
 
-    // GET api/nationals
+    // GET api/states
     [HttpGet]
     public ActionResult<IEnumerable<National>> Get()
     {
       return _db.Nationals.ToList();
     }
 
-    // POST api/nationals
+    // POST api/states
     [HttpPost]
-    public void Post([FromBody] National national)
+    public void Post([FromBody] National state)
     {
-      _db.Nationals.Add(national);
+      _db.Nationals.Add(state);
       _db.SaveChanges();
     }
 
-    // GET api/nationals
+    // GET api/states
     [HttpGet("{id}")]
     public ActionResult<National> Get(int id)
     {
       return _db.Nationals.FirstOrDefault(entry => entry.NationalId == id);
     }
 
-    // PUT api/nationals
+    // PUT api/states
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] National national)
+    public void Put(int id, [FromBody] National state)
     {
-      national.NationalId = id;
-      _db.Entry(national).State = EntityState.Modified;
+      state.NationalId = id;
+      _db.Entry(state).State = EntityState.Modified;
       _db.SaveChanges();
     }
 
-    // DELETE api/nationals
+    // DELETE api/states
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
-      var nationalToDelete = _db.Nationals.FirstOrDefault(entry => entry.NationalId == id);
-      _db.Nationals.Remove(nationalToDelete);
+      var stateToDelete = _db.Nationals.FirstOrDefault(entry => entry.NationalId == id);
+      _db.Nationals.Remove(stateToDelete);
       _db.SaveChanges();
     }
   }
--- Parks/Models/ParksContext.cs
+++ Parks/Models/ParksContext.cs
@@ -21,6 +21,15 @@ namespace Parks.Models
                 new State { StateId = 3, StateParkName = "Ecola State Park", StateName = "Oregon", StateParkYear = 1932 },
                 new State { StateId = 4, StateParkName = "Tettegouche State Park", StateName = "Minnesota", StateParkYear = 1979 },
                 new State { StateId = 5, StateParkName = "Valley of Fire State Park", StateName = "Nevada", StateParkYear = 1935 }
+            );
+
+        builder.Entity<National>()
+            .HasData(
+                new National { NationalId = 1, NationalParkName = "Glacier National Park", NationalName = "USA", NationalParkYear = 1910 },
+                new National { NationalId = 2, NationalParkName = "Grand Canyon National Park", NationalName = "USA", NationalParkYear = 1919 },
+                new National { NationalId = 3, NationalParkName = "Crater Lake National Park", NationalName = "USA", NationalParkYear = 1902 },
+                new National { NationalId = 4, NationalParkName = "Yellowstone National Park", NationalName = "USA", NationalParkYear = 1872 },
+                new National { NationalId = 5, NationalParkName = "Yosemite National Park", NationalName = "USA", NationalParkYear = 1890 }
                 
             );
         }
