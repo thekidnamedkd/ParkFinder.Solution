3fbf38733824cf6b2d0822f07378e01633dc6882
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-21 10:23:57 -0700

    add migrations Initial and update database.

---
+++ Parks/Migrations/20200821171426_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parks.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nationals",
+                columns: table => new
+                {
+                    NationalId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NationalParkName = table.Column<string>(nullable: true),
+                    NationalName = table.Column<string>(nullable: true),
+                    NationalParkYear = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nationals", x => x.NationalId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "States",
+                columns: table => new
+                {
+                    StateId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StateParkName = table.Column<string>(nullable: true),
+                    StateName = table.Column<string>(nullable: true),
+                    StateParkYear = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_States", x => x.StateId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nationals");
+
+            migrationBuilder.DropTable(
+                name: "States");
+        }
+    }
+}
